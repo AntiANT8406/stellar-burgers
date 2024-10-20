15145953fb10093e6fa97316dc2ddd7debb82956
AntiANT8406 <AntiANT8406@yandex.ru>
2024-10-20 20:42:31 +0300

    feat: Добавлены маршруты без переадресации

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,45 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='*' element={<NotFound404 />} />
+
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal title='Детали ингредиента' onClose={() => {}}>
+            <IngredientDetails />
+          </Modal>
+        }
+      />
+      {/* <Route path='/feed/:number' element={<OrderInfo />} />
+      <Route path='/profile/orders/:id' element={<OrderInfo />} /> */}
+    </Routes>
   </div>
 );
 
